85f26789a4fd778a25fcd44083272798299984cb
Vanessa dos Santos Berti <1900865@SALAS.AULAS>
2019-09-13 20:25:21 -0300

    Primeiro Commit

---
+++ com/kuma/operacoes.py
@@ -0,0 +1,14 @@
+classe Operacoes():
+
+def soma(self,valores):
+	val = 0
+	for v in valores:
+		val = val + v
+		
+	return val 
+	
+def subtracao(self, valores):
+	val = 0
+	for v in valores:
+		val = val - v
+	return val 
